4edcb505d8b8daca2e6a9fe7862823b8617532ca
Josh <15219491+jkaps9@users.noreply.github.com>
2025-10-25 21:53:58 -0400

    form shows success message on submit

--- src/main.js
+++ src/main.js
@@ -17,3 +17,20 @@ if (themeToggle && body) {
     body.classList.toggle("dark");
   });
 }
+
+// Form
+const form = document.querySelector(".newsletter__form");
+
+if (form) {
+  const successMessage = form.querySelector(".success-message");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    if (successMessage && form.checkValidity()) {
+      successMessage.classList.add("active");
+    }
+  });
+
+  form.addEventListener("");
+}
